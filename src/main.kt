6857b2e7ec18012a63d309c4cde4f8ebd5b82240
DragonOfDissent <dvpsherman@gmail.com>
2021-09-19 13:57:45 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>){
+    // Create variables
+    var myByte: Byte = 7
+    var myShort: Short = 98
+    var myInt: Int = 10
+    var myLong = 40234234L
+    var myFloat: Float = .1F
+    var myDouble: Double = 234234.2234
+    var myBoolean: Boolean = false
+    var myChar: Char = 'D'
+
+    // Assign new values to the variables
+    myByte = 6
+    myShort = 97
+    myInt = 9
+    myLong = 40234233
+    myFloat = .09F
+    myDouble = 234234.2233
+    myBoolean = true
+    myChar = 'C'
+
+    // Print the variables
+    println("Byte Example: $myByte")
+    println("Short Example: $myShort")
+    println("Int Example: $myInt")
+    println("Long Example: $myLong")
+    println("Float Example: $myFloat")
+    println("Double Example: $myDouble")
+    println("Boolean Example: $myBoolean")
+    println("Char Example: $myChar")
+}
